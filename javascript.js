c13314f886f441112ad33f0ab623ef27383ed434
Kim Oflash <105842823+kimoflash@users.noreply.github.com>
2023-04-07 17:24:05 +0800

    Separate HTML, CSS, and JavaScript files

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let div = document.querySelector('div');
+//container.setAttribute('style', 'color: white; background: #F88379; text-align: center;');
+
+div.setAttribute('id', 'bar');
+div.classList.add('foo');
